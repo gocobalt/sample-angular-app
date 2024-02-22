6ef278652e7f98d37cb3a453e01f9eeaa3be5fde
TRACTION <19631364+iamtraction@users.noreply.github.com>
2024-02-22 17:04:55 +0530

    app: accept linked token from user
    Signed-off-by: TRACTION <19631364+iamtraction@users.noreply.github.com>

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,40 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MatButton } from "@angular/material/button";
+import { MatFormField, MatHint, MatInput, MatLabel } from "@angular/material/input";
 import { CobaltConfig } from '@cobaltio/ngx-cobalt';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CobaltConfig],
+  imports: [
+    // CommonModule,
+    CobaltConfig,
+    MatButton,
+    MatFormField,
+    MatHint,
+    MatInput,
+    MatLabel,
+    RouterOutlet,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'angular-sample';
+  token = '';
+  ready = false;
+
+  setToken(event: KeyboardEvent) {
+    this.token = (event.target as HTMLInputElement).value;
+  }
+
+  saveToken() {
+    if (this.ready) {
+      this.token = "";
+      this.ready = false;
+    } else {
+      this.ready = true;
+    }
+  }
 }
